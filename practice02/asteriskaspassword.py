dd755f0cced77704397869d30b7a671b17dbe7d0
Adetayo <twinebravo@gmail.com>
2024-07-13 07:59:14 +0100

    Added python file

---
+++ practice02/asteriskaspassword.py
@@ -0,0 +1,10 @@
+password = input("Enter password: ")
+
+length_of_password = len(password)
+
+asterisk = "*"
+replace_password = (length_of_password * asterisk)
+print(replace_password)
+
+#for asterisk in range(length_of_password):
+	#print("*", end="")
